fd84abcf714e98ec8e2a275a5e968ca646bf6432
Abdella <abdella.shukur2@gmail.com>
2020-06-26 18:30:17 -0400

    added bootstrap components

--- src/componets/cards.js
+++ src/componets/cards.js
@@ -1,9 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Cards extends Component {
-    render() { 
-        return ( <h4>Cards</h4> );
-    }
+  render() {
+    return (
+      <div class="card-group">
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+        <div class="card">
+          <img class="card-img-top" src="..." alt="Card cap" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Cards;
--- src/componets/footer.js
+++ src/componets/footer.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
+import React, { Component } from "react";
 
 class Footer extends Component {
-    // state = {  }
-    render() { 
-        return ( 
-<h4>Footer</h4>
-        );
-    }
+  // state = {  }
+  render() {
+    return (
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" href="#home">
+            Home
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#movies">
+            Movies
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">
+            Members
+          </a>
+        </li>
+      </ul>
+    );
+  }
 }
- 
+
 export default Footer;
